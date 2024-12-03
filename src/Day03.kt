4c81a708e6377eae6408a4b8725877e01f607b94
volo <vruzhansky@gmail.com>
2024-12-03 09:52:29 +0100

    Day 03

---
+++ src/Day03.kt
@@ -0,0 +1,39 @@
+private const val DAY = "03"
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val rex = Regex("mul\\((\\d+),(\\d+)\\)")
+
+        var total = 0
+        rex.findAll(input.joinToString(separator = ""))
+            .map { it.groupValues }
+            .forEach { (_, arg1, arg2) -> total += arg1.toInt() * arg2.toInt() }
+        return total
+    }
+
+    fun part2(input: List<String>): Int {
+        val rex = Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+
+        var total = 0
+        var doIt = true
+        rex.findAll(input.joinToString(separator = "")).forEach { res ->
+            val vals = res.groupValues
+            val cmd = vals[0]
+            when {
+                cmd.startsWith("mul") && doIt -> total += vals[1].toInt() * vals[2].toInt()
+                cmd.startsWith("don't") -> doIt = false
+                cmd.startsWith("do") -> doIt = true
+            }
+        }
+        return total
+    }
+
+    val testInput = readInput("Day${DAY}_test")
+//    check(part1(testInput) == 161)
+    check(part2(testInput) == 48)
+
+    val input = readInput("Day${DAY}")
+    part1(input).println()
+    part2(input).println()
+}
