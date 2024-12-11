156f34306bbef19968b60fb7de0db12167ec6f0d
volo <vruzhansky@gmail.com>
2024-12-11 22:24:59 +0100

    Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -4,21 +4,21 @@ fun main() {
 
     data class Num(val num: Long, val lvl: Int)
 
-    fun calc(value: Long, lvl: Int, maxLvl: Int, cache: MutableMap<Num, Long>): Long =
-        if (maxLvl == lvl) {
+    fun calc(value: Long, lvl: Int, cache: MutableMap<Num, Long>): Long =
+        if (lvl == 0) {
             1
         } else {
             cache.getOrPut(Num(value, lvl)) {
                 when {
-                    value == 0L -> calc(1L, lvl + 1, maxLvl, cache)
+                    value == 0L -> calc(1L, lvl - 1, cache)
                     value.toString().length % 2 == 0 -> {
                         val strNum = value.toString()
                         val mid = strNum.length / 2
-                        calc(strNum.substring(0, mid).toLong(), lvl + 1, maxLvl, cache) +
-                                calc(strNum.substring(mid).toLong(), lvl + 1, maxLvl, cache)
+                        calc(strNum.substring(0, mid).toLong(), lvl - 1, cache) +
+                                calc(strNum.substring(mid).toLong(), lvl - 1, cache)
                     }
 
-                    else -> calc(value * 2024, lvl + 1, maxLvl, cache)
+                    else -> calc(value * 2024, lvl - 1, cache)
                 }
             }
         }
@@ -28,7 +28,7 @@ fun main() {
 
         val cache = mutableMapOf<Num, Long>()
         return arr.fold(0L) { acc: Long, l: Long ->
-            acc + calc(l, 0, 25, cache)
+            acc + calc(l, 25, cache)
         }
     }
 
@@ -37,7 +37,7 @@ fun main() {
 
         val cache = mutableMapOf<Num, Long>()
         return arr.fold(0L) { acc: Long, l: Long ->
-            acc + calc(l, 0, 75, cache)
+            acc + calc(l, 75, cache)
         }
     }
 
